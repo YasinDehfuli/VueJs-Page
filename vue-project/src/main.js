8c2a395727cd2567b424d2ddbcaac3a245912933
YasinDehfuli <Ydehfuli@Gmail.Com>
2022-09-27 17:41:31 +0330

    First Vue Js Click ++ To H1

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 
-import './assets/main.css'
 
 createApp(App).mount('#app')
